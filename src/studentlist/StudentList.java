6648e8c17fef5329a046647b8aeb19cf1edc7943
MananPathak13 <pathaman@sheridancollege.ca>
2020-01-23 10:07:55 -0500

    Fixed the complication

--- src/studentlist/StudentList.java
+++ src/studentlist/StudentList.java
@@ -3,8 +3,8 @@ package studentlist;
 import java.util.Scanner;
 
 /**
- * This class our first Example class for review
- *modified on jan 23
+ * This class our first Example class for review modified on jan 23
+ *
  * @author Paul Bonenfant
  */
 public class StudentList {
@@ -13,30 +13,29 @@ public class StudentList {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+        Scanner input = new Scanner(System.in);
         // removed the code
-        
+
         // add a blank line
         System.out.println();
-        
+
         System.out.println("Now we create our Students");
-        
+
         Student[] students = new Student[3];
-        
-        for(int i = 0; i < 3; i++) {
-            System.out.println("Enter Student Name");    
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Enter Student Name");
             String name = input.nextLine();
             Student student = new Student(name);
             students[i] = student;
         }
-        
+
         System.out.println("Let's print them back");
-        
-        for(Student student: students) {
+
+        for (Student student : students) {
             System.out.println(student.getName());
         }
-        
+
     }
 
 }
-
